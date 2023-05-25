b260e22cc33d0f83775d70374023e02ac04b6733
Statusplus2015 <134066616+Statusplus2015@users.noreply.github.com>
2023-05-25 22:05:32 +0600

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,628 @@
+const questions = [
+    {
+        question: "I _____ from France.",
+        optionA: "is",
+        optionB: "are ",
+        optionC: "am",
+        optionD: "be",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "This is my friend. ______ name is Peter",
+        optionA: "Her",
+        optionB: "Our",
+        optionC: "Yours",
+        optionD: "His",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Mike is _______.",
+        optionA: "my sister’s friend",
+        optionB: "friend my sister",
+        optionC: "friend from my sister",
+        optionD: "my sister friend’s",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "My brother is _____ artist.",
+        optionA: "the",
+        optionB: "an",
+        optionC: "a",
+        optionD: "-",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "_______ 20 desks in the classroom.",
+        optionA: "This is",
+        optionB: "There is",
+        optionC: "They are",
+        optionD: "There are",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Paul ________ romantic films.",
+        optionA: "likes not",
+        optionB: "don’t like",
+        optionC: "doesn’t like",
+        optionD: "isn’t likes",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Sorry, I can’t talk. I _______ right now.	",
+        optionA: "driving",
+        optionB: "‘m driving",
+        optionC: "drives",
+        optionD: "drive",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "She _________ at school last week.",
+        optionA: "didn't be",
+        optionB: "weren’t",
+        optionC: "wasn’t",
+        optionD: "isn’t",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "I ________ the film last night.",
+        optionA: "like",
+        optionB: "likes",
+        optionC: "liking",
+        optionD: "liked",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "___________ a piece of cake? No, thank you.",
+        optionA: "Do you like",
+        optionB: "Would you like",
+        optionC: "Want you",
+        optionD: "Are you like",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "The living room is _________ than the bedroom.",
+        optionA: "more big",
+        optionB: "more bigger",
+        optionC: "biggest",
+        optionD: "bigger",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "The car is very old. We’re going ______ a new car soon. ",
+        optionA: "to buy ",
+        optionB: "buying ",
+        optionC: "to will buy ",
+        optionD: " buy",
+        correctOption: "optionA"
+    },
+
+
+    {
+        question: "Jane is a vegetarian. She _________ meat. ",
+        optionA: " sometimes eats",
+        optionB: "never eats ",
+        optionC: "often eats ",
+        optionD: "usually eats ",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "There aren’t _________ buses late in the evening.	 ",
+        optionA: " some",
+        optionB: "any",
+        optionC: "no ",
+        optionD: "a ",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "The car park is __________ to the restaurant. ",
+        optionA: " next ",
+        optionB: "opposite ",
+        optionC: " behind",
+        optionD: " in front",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Sue _______ shopping every day. ",
+        optionA: "is going ",
+        optionB: "go ",
+        optionC: "going ",
+        optionD: "goes",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "They _________ in the park when it started to rain heavily.",
+        optionA: "walked ",
+        optionB: " were walking",
+        optionC: "were walk ",
+        optionD: " are walking ",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "______ seen fireworks before? ",
+        optionA: " Did you ever",
+        optionB: "Are you ever ",
+        optionC: " Have you ever",
+        optionD: "Do you ever ",
+        correctOption: "optionC"
+    },
+
+    {
+        question: " We’ve been friends _______ many years.",
+        optionA: "since ",
+        optionB: "from  ",
+        optionC: "during ",
+        optionD: " for",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "You _________ pay for the tickets. They’re free.",
+        optionA: " have to",
+        optionB: " don’t have",
+        optionC: "don’t need to ",
+        optionD: " doesn’t have to",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Jeff was ill last week and he ________ go out. ",
+        optionA: "needn't ",
+        optionB: "can’t ",
+        optionC: "mustn’t ",
+        optionD: "couldn’t ",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "These are the photos __ I took on holiday. ",
+        optionA: "which ",
+        optionB: " who",
+        optionC: " what",
+        optionD: " where",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "We’ll stay at home if it ______ this afternoon. ",
+        optionA: " raining",
+        optionB: " rains",
+        optionC: " will rain",
+        optionD: " rain",
+        correctOption: "optionB"
+    },
+
+    {
+        question: " He doesn’t smoke now, but he _______ a lot when he was young.",
+        optionA: "has smoked ",
+        optionB: "smokes ",
+        optionC: "used to smoke ",
+        optionD: "was smoked",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Mark plays football _________ anyone else I know. ",
+        optionA: " more good than",
+        optionB: "as better as",
+        optionC: "best than ",
+        optionD: "better than ",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "I promise I _______ you as soon as I’ve finished this cleaning. ",
+        optionA: "will help ",
+        optionB: "am helping ",
+        optionC: "going to help ",
+        optionD: "have helped",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "This town _____ by lots of tourists during the summer.",
+        optionA: "visits ",
+        optionB: "visited",
+        optionC: " is visiting",
+        optionD: "is visited ",
+        correctOption: "optionD"
+    },
+
+    {
+        question: " He said that his friends _______ to speak to him after they lost the football match.",
+        optionA: "not want ",
+        optionB: "weren’t ",
+        optionC: "didn’t want ",
+        optionD: "aren’t wanting ",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "How about _______ to the cinema tonight? ",
+        optionA: "going  ",
+        optionB: "go ",
+        optionC: "to go ",
+        optionD: "for going ",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Excuse me, can you ________ me the way to the station, please? ",
+        optionA: "give ",
+        optionB: "take ",
+        optionC: "tell ",
+        optionD: "say ",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "I wasn’t interested in the performance very much. _______.",
+        optionA: "I didn’t, too. ",
+        optionB: "Neither was I ",
+        optionC: "Nor I did. ",
+        optionD: "So I wasn’t. ",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Take a warm coat, ____ you might get very cold outside. ",
+        optionA: "otherwise ",
+        optionB: "in case ",
+        optionC: "so that ",
+        optionD: "in order to ",
+        correctOption: "optionA"
+    },
+
+    {
+        question: " ________ this great book and I can’t wait to see how it ends. ",
+        optionA: "I don’t read ",
+        optionB: "I’ve read ",
+        optionC: "I’ve been reading ",
+        optionD: "I read ",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "What I like more than anything else _________ at weekends. ",
+        optionA: "playing golf ",
+        optionB: "to play golf ",
+        optionC: "is playing golf ",
+        optionD: "is play golf ",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "She _____ for her cat for two days when she finally found it in the garage.  ",
+        optionA: "looked ",
+        optionB: "had been looked ",
+        optionC: "had been looking ",
+        optionD: "were looking ",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "We won’t catch the plane ____ we leave home now! Please hurry up! ",
+        optionA: "if ",
+        optionB: "providing that ",
+        optionC: "except ",
+        optionD: "unless ",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "If I hadn’t replied to your email, I____ here with you now. ",
+        optionA: "can’t be ",
+        optionB: "wouldn’t be ",
+        optionC: "won’t be ",
+        optionD: "haven’t been ",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Do you think you ___________ with my mobile phone soon? I need to make a call.  ",
+        optionA: "finish ",
+        optionB: "are finishing ",
+        optionC: "will have finished",
+        optionD: "are finished ",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "I don’t remember mentioning ___________ dinner together tonight.  ",
+        optionA: "go for ",
+        optionB: "you going to ",
+        optionC: "to go for ",
+        optionD: "going for ",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Was it Captain Cook ______ New Zealand? ",
+        optionA: "who discovered ",
+        optionB: "discovered ",
+        optionC: "that discover ",
+        optionD: "who was discovering ",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "You may not like the cold weather here, but you’ll have to ______, I’m afraid.",
+        optionA: "tell it off ",
+        optionB: "sort itself out ",
+        optionC: "put up with it ",
+        optionD: "put it off ",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "It’s cold so you should __________ on a warm jacket. ",
+        optionA: "put ",
+        optionB: "wear ",
+        optionC: "dress ",
+        optionD: "take ",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Paul will look _____ our dogs while we’re on holiday. ",
+        optionA: "at ",
+        optionB: "for",
+        optionC: "into ",
+        optionD: "after ",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "She ___________ a lot of her free time reading. ",
+        optionA: "does  ",
+        optionB: "spends ",
+        optionC: "has ",
+        optionD: "makes  ",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Hello, this is Simon. Could I ___________ to Jane, please? ",
+        optionA: "say  ",
+        optionB: "tell ",
+        optionC: "call ",
+        optionD: "speak ",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "They’re coming to our house ___________ Saturday. ",
+        optionA: "in ",
+        optionB: "at ",
+        optionC: "on ",
+        optionD: "with ",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "I think it’s very easy to ______ debt these days. ",
+        optionA: "go into ",
+        optionB: "become ",
+        optionC: "go down to ",
+        optionD: "get into ",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Come on! Quick! Let’s get _______!	 ",
+        optionA: "highlight ",
+        optionB: "cracking ",
+        optionC: "massive ",
+        optionD: " with immediate effect",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "I phoned her ______ I heard the news. ",
+        optionA: "minute ",
+        optionB: "during ",
+        optionC: "by the time ",
+        optionD: "the moment ",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "I feel very ______. I’m going to go to bed! ",
+        optionA: "nap ",
+        optionB: "asleep ",
+        optionC: "sleepy ",
+        optionD: "sleeper ",
+        correctOption: "optionC"
+    }
+]
+
+
+let shuffledQuestions = [] //empty array to hold shuffled selected questions out of all available questions
+
+function handleQuestions() { 
+    //function to shuffle and push 49 questions to shuffledQuestions array
+//app would be dealing with 49 questions per session
+    while (shuffledQuestions.length <= 49) {
+        const random = questions[Math.floor(Math.random() * questions.length)]
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random)
+        }
+    }
+}
+
+
+let questionNumber = 1 //holds the current question number
+let playerScore = 0  //holds the player score
+let wrongAttempt = 0 //amount of wrong answers picked by player
+let indexNumber = 0 //will be used in displaying next question
+
+// function for displaying next question in the array to dom
+//also handles displaying players and quiz information to dom
+function NextQuestion(index) {
+    handleQuestions()
+    const currentQuestion = shuffledQuestions[index]
+    document.getElementById("question-number").innerHTML = questionNumber
+    document.getElementById("player-score").innerHTML = playerScore
+    document.getElementById("display-question").innerHTML = currentQuestion.question;
+    document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
+    document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
+    document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
+    document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
+
+}
+
+
+function checkForAnswer() {
+    const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
+    const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
+    const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+    let correctOption = null
+
+    options.forEach((option) => {
+        if (option.value === currentQuestionAnswer) {
+            //get's correct's radio input with correct answer
+            correctOption = option.labels[0].id
+        }
+    })
+
+    //checking to make sure a radio input has been checked or an option being chosen
+    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+        document.getElementById('option-modal').style.display = "flex"
+    }
+
+    //checking if checked radio button is same as answer
+    options.forEach((option) => {
+        if (option.checked === true && option.value === currentQuestionAnswer) {
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            playerScore++ //adding to player's score
+            indexNumber++ //adding 1 to index so has to display next question..
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+
+        else if (option.checked && option.value !== currentQuestionAnswer) {
+            const wrongLabelId = option.labels[0].id
+            document.getElementById(wrongLabelId).style.backgroundColor = "red"
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            wrongAttempt++ //adds 1 to wrong attempts 
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+    })
+}
+
+
+
+//called when the next button is called
+function handleNextQuestion() {
+    checkForAnswer() //check if player picked right or wrong option
+    unCheckRadioButtons()
+    //delays next question displaying for a second just for some effects so questions don't rush in on player
+    setTimeout(() => {
+        if (indexNumber <= 49) {
+//displays next question as long as index number isn't greater than 49, remember index number starts from 0, so index 49 is question 50
+            NextQuestion(indexNumber)
+        }
+        else {
+            handleEndGame()//ends game if index number greater than 49 meaning we're already at the 10th question
+        }
+        resetOptionBackground()
+    }, 1000);
+}
+
+//sets options background back to null after display the right/wrong colors
+function resetOptionBackground() {
+    const options = document.getElementsByName("option");
+    options.forEach((option) => {
+        document.getElementById(option.labels[0].id).style.backgroundColor = ""
+    })
+}
+
+// unchecking all radio buttons for next question(can be done with map or foreach loop also)
+function unCheckRadioButtons() {
+    const options = document.getElementsByName("option");
+    for (let i = 0; i < options.length; i++) {
+        options[i].checked = false;
+    }
+}
+
+// function for when all questions being answered
+function handleEndGame() {
+    let remark = null
+    let remarkColor = null
+
+    // condition check for player remark and remark color
+    if (playerScore <= 15) {
+        remark = "Your level is: Beginner"
+        remarkColor = "red"
+    }
+    else if (playerScore >= 16 && playerScore < 24) {
+        remark = "Your level is: Elementary"
+        remarkColor = "orange"
+    }
+    else if (playerScore >= 25 && playerScore < 32) {
+        remark = "Your level is: Pre-intermediate"
+        remarkColor = "orange"
+    }
+    else if (playerScore >= 33 && playerScore < 39) {
+        remark = "Your level is: Intermediate"
+        remarkColor = "orange"
+    }
+    else if (playerScore >= 40 && playerScore < 45) {
+        remark = "Your level is: Upper Intermediate"
+        remarkColor = "orange"
+    }
+    else if (playerScore >= 46) {
+        remark = "Your level is: Advanced"
+        remarkColor = "green"
+    }
+    const playerGrade = (playerScore / 50) * 100
+
+    //data to display to score board
+    document.getElementById('remarks').innerHTML = remark
+    document.getElementById('remarks').style.color = remarkColor
+    document.getElementById('grade-percentage').innerHTML = playerGrade
+    document.getElementById('wrong-answers').innerHTML = wrongAttempt
+    document.getElementById('right-answers').innerHTML = playerScore
+    document.getElementById('score-modal').style.display = "flex"
+    //hide questions container
+    document.getElementById('quizQuestions').style.display="none"
+
+}
+
+//closes score modal, resets game and reshuffles questions
+function closeScoreModal() {
+    questionNumber = 1
+    playerScore = 0
+    wrongAttempt = 0
+    indexNumber = 0
+    shuffledQuestions = []
+    NextQuestion(indexNumber)
+    document.getElementById('score-modal').style.display = "none"
+    document.getElementById('quizQuestions').style.display= "flex"
+}
+
+//function to close warning modal
+function closeOptionModal() {
+    document.getElementById('option-modal').style.display = "none"
+}
